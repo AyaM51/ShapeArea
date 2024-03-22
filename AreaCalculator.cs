ea5a829c587d1d53295d44df257db01729250fd6
”Aya” <ayasmailike@gmail.com>
2024-03-22 21:13:41 +0600

    initial

---
+++ AreaCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+    public class AreaCalculator
+    {
+        private Dictionary<string, IShape> shapes = new()
+        {
+            { ShapeNames.Circle, new Circle() },
+            { ShapeNames.Triangle, new Triangle() },
+        };
+        public double GetAreaBySides(string shape_type, params double[] sides)
+        {
+            if (!shapes.ContainsKey(shape_type)) throw new NullReferenceException("shape does not exist");
+            Utilites.CheckZeroNegative(sides);
+            shapes[shape_type].ValidateInput(sides);
+            return shapes[shape_type].GetArea(sides);
+        }
+
+        public void AddUserShape(IShape s)
+        {
+            if (String.IsNullOrEmpty(s.GetShapeName()))
+            {
+                throw new InvalidOperationException("shape must have name");
+            }
+            if (shapes.ContainsKey(s.GetShapeName()))
+            {
+                throw new InvalidOperationException("shape already exist");
+            }
+            shapes.Add(s.GetShapeName(), s);
+        }
+    }
+}
---
+++ Circle.cs
@@ -0,0 +1,18 @@
+﻿namespace ShapeArea
+{
+    internal class Circle : IShape
+    {
+        public string GetShapeName()
+        {
+            return ShapeNames.Circle;
+        }
+        public void ValidateInput(params double[] radius)
+        {
+            if (radius.Length != 1) throw new ArgumentException("circle must have one 'radius' value");
+        }
+        public double GetArea(params double[] radius)
+        {
+            return Math.PI * Math.Pow(radius[0], 2);
+        }
+    }
+}
